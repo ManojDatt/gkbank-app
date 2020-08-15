01553f83d09c5bffc9a4aa5998dc9a88e4cc5dbc
Your Name <you@example.com>
2020-08-15 19:53:15 +0530

    df

--- GkBank/celery.py
+++ GkBank/celery.py
@@ -23,5 +23,4 @@ def send_transaction_mail(trans_type, json_data, email):
 	subject = f'Your transaction for {trans_type} processed.'
 	html_message = render_to_string('transaction_mail.html',json_data)
 	plain_message = strip_tags(html_message)
-	from_email = 'From <from@example.com>'
 	send_mail(subject, plain_message, 'support@gkmit.com', [email], fail_silently=False)
